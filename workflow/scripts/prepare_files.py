cff15216f9d4bfe5e2e4d7888c8c8212c17ec3b7
andrewgull <andrewgull87@gmail.com>
2021-08-25 14:00:21 +0200

    rewritten with argparse and get_args()

--- workflow/scripts/prepare_files.py
+++ workflow/scripts/prepare_files.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
-
-# this script creates soft links to read files
-# Give them proper names!
-# and joins a strain's nanopore reads into a single file (former functionality of the rule join_nanopore)
-# naming scheme is as follows
-# Illumina: STRAIN_[1, 2].fastq.gz
-# Nanopore: STRAIN_all.fastq.gz
-# it is assumed that you've copied files from ARGOS to ../data_raw
+"""
+This script creates soft links to read files
+Give them proper names!
+and joins a strain's nanopore reads into a single file (former functionality of the rule join_nanopore)
+naming scheme is as follows
+Illumina: STRAIN_[1, 2].fastq.gz
+Nanopore: STRAIN_all.fastq.gz
+It is assumed that you've copied files from ARGOS to ../data_raw
+"""
 import os
 import glob
 import subprocess
-import sys
 from tqdm import tqdm
+import argparse
 
-help_message = 'Script for linking read files, renaming them properly and joining Nanopore reads for each strain.\n' \
-               'Usage: prepare_files.py <strains_file> <threads>'
 
-# PARAMETERS
-try:
-    strains_file = sys.argv[1]
-    threads = sys.argv[2]
-except IndexError:
-    print(help_message)
-    sys.exit()
+def get_args():
+    """
+    Get command line arguments
+    """
+
+    parser = argparse.ArgumentParser(
+        description="This script creates properly named soft links in ./data_raw\n"
+                    "And concatenates Nanopore reads into one file prefixed with '_all'\n"
+                    "Naming scheme is as follows:\n'STRAIN_[1,2].fq.gz' for Illumina reads\n"
+                    "'STRAIN_all.fastq.gz' for Nanopore reads\n"
+                    "It is assumed that all read files have been copied to ./data_raw",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    parser.add_argument("strains", metavar="<strains file>",
+                        help="File with strain names, one name per line")
+    parser.add_argument("threads", metavar="<no. threads>", help="number of threads to use in pigz")
+    return parser.parse_args()
 
 
 def new_name(path):
@@ -40,89 +50,96 @@ def new_name(path):
     return file_name
 
 
-# change working directory
-# os.chdir("..")
-# now raw data is in ./data_raw
+def main():
+    # PARAMETERS
+    args = get_args()
+    strains_file = args.strains
+    threads = args.threads
 
-# 1. COMPRESSING FILES
-# check uncompressed files and compress them
-# to find fastq and fq extensions
-print("1. Checking uncompressed files...\n")
+    # 1. COMPRESSING FILES
+    # check uncompressed files and compress them
+    # to find fastq and fq extensions
+    print("1. Checking uncompressed files...\n")
 
-# this line gives you all .*q files regardless of number of directories in DA*
-uncomp_files = glob.glob("data_raw/DA*/**/*.*q", recursive=True)
-if len(uncomp_files) > 0:
-    for line in tqdm(uncomp_files):
-        subprocess.run(["pigz", "-p", "%s" % threads, line])
-else:
-    print("No uncompressed files found")
-
-print("2. Collecting compressed files and creating renamed symlinks...\n")
-
-# to get a list of strains use provided file
-# strains = glob.glob("data_raw/DA*")
-with open(strains_file, 'r') as f:
-    strains = [line.rstrip() for line in f.readlines()]
-
-# 2. CREATING SYMLINKS WITH THE RIGHT NAMES
-for strain in strains:
-    # to get a full path to each of GZ files in a given strain:
-    read_files = glob.glob("data_raw/%s/**/*.gz" % strain, recursive=True)
-    # beginning of each line is a strain name, ending - current filename
-    # in the filename - there's a barcode and replicate/read number
-    # I need only 2nd barcode from strains with two barcode directories
-    # and the only barcode from strains with one barcode directories
-    illumina_files = [line for line in read_files if "Illumina" in line]
-    nanopore_files = [line for line in read_files if "Nanopore" in line]
-    # there can be a case when there's no nanopore or illumina files - we report these cases
-    if len(illumina_files) == 0:
-        print("No Illumina reads found for %s" % strain)
-    if len(nanopore_files) == 0:
-        print("No Nanopore reads found for %s" % strain)
-        nanopore_clean = list()
+    # this line gives you all .*q files regardless of number of directories in DA*
+    uncomp_files = glob.glob("data_raw/DA*/**/*.*q", recursive=True)
+    if len(uncomp_files) > 0:
+        for line in tqdm(uncomp_files):
+            subprocess.run(["pigz", "-p", "%s" % threads, line])
     else:
-        # filter out nanopore files you don't need
-        two_barcodes = [line for line in nanopore_files if line.count("barcode") == 3]
-        if len(two_barcodes) == 0:
-            nanopore_clean = [line for line in nanopore_files if line.count("barcode") == 2]
-            # use these files for symlinks
+        print("No uncompressed files found")
+
+    print("2. Collecting compressed files and creating renamed symlinks...\n")
+
+    # to get a list of strains use provided file
+    # strains = glob.glob("data_raw/DA*")
+    with open(strains_file, 'r') as f:
+        strains = [line.rstrip() for line in f.readlines()]
+
+    # 2. CREATING SYMLINKS WITH THE RIGHT NAMES
+    for strain in strains:
+        # to get a full path to each of GZ files in a given strain:
+        read_files = glob.glob("data_raw/%s/**/*.gz" % strain, recursive=True)
+        # beginning of each line is a strain name, ending - current filename
+        # in the filename - there's a barcode and replicate/read number
+        # I need only 2nd barcode from strains with two barcode directories
+        # and the only barcode from strains with one barcode directories
+        illumina_files = [line for line in read_files if "Illumina" in line]
+        nanopore_files = [line for line in read_files if "Nanopore" in line]
+        # there can be a case when there's no nanopore or illumina files - we report these cases
+        if len(illumina_files) == 0:
+            print("No Illumina reads found for %s" % strain)
+        if len(nanopore_files) == 0:
+            print("No Nanopore reads found for %s" % strain)
+            nanopore_clean = list()
         else:
-            # use two_barcodes for symlinks
-            nanopore_clean = two_barcodes
+            # filter out nanopore files you don't need
+            two_barcodes = [line for line in nanopore_files if line.count("barcode") == 3]
+            if len(two_barcodes) == 0:
+                nanopore_clean = [line for line in nanopore_files if line.count("barcode") == 2]
+                # use these files for symlinks
+            else:
+                # use two_barcodes for symlinks
+                nanopore_clean = two_barcodes
 
-    # join illumina and nanopore lists
-    read_files = illumina_files + nanopore_clean
-    # iterate and create symlinks
-    for line in read_files:
-        line_new_name = new_name(line)
-        # now, when you have a new name, rename the file (create a soft link)
-        # put links into Nanopore or Illumina directories, respectively
-        # for symlinks you need absolute paths
-        cwd = os.getcwd()
-        source = "/".join([cwd, line])
-        # create destination - absolute path
-        if "Nanopore" in line:
-            destination = "/".join([cwd, "data_raw", strain, "Nanopore", line_new_name])
+        # join illumina and nanopore lists
+        read_files = illumina_files + nanopore_clean
+        # iterate and create symlinks
+        for line in read_files:
+            line_new_name = new_name(line)
+            # now, when you have a new name, rename the file (create a soft link)
+            # put links into Nanopore or Illumina directories, respectively
+            # for symlinks you need absolute paths
+            cwd = os.getcwd()
+            source = "/".join([cwd, line])
+            # create destination - absolute path
+            if "Nanopore" in line:
+                destination = "/".join([cwd, "data_raw", strain, "Nanopore", line_new_name])
+            else:
+                destination = "/".join([cwd, "data_raw", strain, "Illumina", line_new_name])
+            # create link
+            try:
+                os.symlink(source, destination)
+            except FileExistsError:
+                print("File exists in destination: %s " % destination)
+
+    # 3. JOINING NANOPORE READS
+    # former rule 'join_nanopore': "zcat {input} | pigz -c -p {threads} > {output}"
+    print("3. Joining Nanopore reads...")
+
+    for strain in tqdm(strains):
+        # strain looks like 'DA62920'
+        path = "data_raw/" + strain + "/Nanopore"
+        # strain_name = strain.split("/")[-1]
+        if os.path.isfile(path + "/" + "%s_all.fastq.gz" % strain):
+            print("Joined Nanopore file for strain %s exists" % strain)
         else:
-            destination = "/".join([cwd, "data_raw", strain, "Illumina", line_new_name])
-        # create link
-        try:
-            os.symlink(source, destination)
-        except FileExistsError:
-            print("File exists in destination: %s " % destination)
+            proc = subprocess.Popen("zcat %s/*.fastq.gz | pigz -c -p %s > %s/%s_all.fastq.gz" %
+                                    (path, threads, path, strain),
+                                    shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = proc.communicate()
+    print("Done!")
 
-# 3. JOINING NANOPORE READS
-# former rule 'join_nanopore': "zcat {input} | pigz -c -p {threads} > {output}"
-print("3. Joining Nanopore reads...")
 
-for strain in tqdm(strains):
-    # strain looks like 'DA62920'
-    path = "data_raw/" + strain + "/Nanopore"
-    # strain_name = strain.split("/")[-1]
-    if os.path.isfile(path + "/" + "%s_all.fastq.gz" % strain):
-        print("Joined Nanopore file for strain %s exists" % strain)
-    else:
-        proc = subprocess.Popen("zcat %s/*.fastq.gz | pigz -c -p %s > %s/%s_all.fastq.gz" % (path, threads, path, strain),
-                                shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = proc.communicate()
-print("Done!")
+if __name__ == '__main__':
+    main()
