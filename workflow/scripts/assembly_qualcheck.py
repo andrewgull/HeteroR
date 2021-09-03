df09f100de29e3b7e5cbe02911d0f78017fa6f3a
andrewgull <andrewgull87@gmail.com>
2021-09-03 17:01:54 +0200

    merge rule quast and rule busco; merge run_busco.py adn run_quast.py into assembly_qualcheck.py

--- workflow/scripts/assembly_qualcheck.py
+++ workflow/scripts/assembly_qualcheck.py
@@ -6,7 +6,7 @@ directive 'run' is also not viable cause than it's impossible to use conda envs
 """
 
 import subprocess
-import snakemake
+# import snakemake
 import os
 
 
@@ -15,11 +15,15 @@ def main(indir, outdir, cpus):
     function to run busco
     """
 
-    # INPUT looks like "assemblies/{strain}" but you need "assemblies/{strain}/assembly.fasta"
+    # run BUSCO
     # busco -m genome -i {input} -o busco_results --out_path {output} -l gammaproteobacteria_odb10 --cpu {threads}"
+    # INPUT looks like "assemblies/{strain}" but you need "assemblies/{strain}/assembly.fasta"
     infile = os.path.join(indir, "assembly.fasta")
     subprocess.call(["busco", "-m", "genome", "-i", infile, "-o", "busco_results", "--out_path", outdir,
-                     "-l", "gammaproteobacteria_odb10", "--cpu", cpus], shell=True)
+                     "-l", "gammaproteobacteria_odb10", "--cpu", cpus])
+    # run QUAST
+    output_dir = os.path.join(outdir, "quast_results")
+    subprocess.call(["quast.py", "-t", cpus, "-o", output_dir, infile])
 
 
-main(indir=snakemake.input[0], outdir=snakemake.output[0], cpus=snakemake.threads)
+main(indir=snakemake.input[0], outdir=snakemake.output[0], cpus=str(snakemake.threads))
