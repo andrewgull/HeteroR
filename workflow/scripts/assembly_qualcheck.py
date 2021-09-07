7c21bca10784452adb2dc28ad82b85aa5e4e7d61
andrewgull <andrewgull87@gmail.com>
2021-09-07 17:27:25 +0200

    fixed logs for busco

--- workflow/scripts/assembly_qualcheck.py
+++ workflow/scripts/assembly_qualcheck.py
@@ -19,8 +19,20 @@ def main(indir, outdir, cpus):
     # busco -m genome -i {input} -o busco_results --out_path {output} -l gammaproteobacteria_odb10 --cpu {threads}"
     # INPUT looks like "assemblies/{strain}" but you need "assemblies/{strain}/assembly.fasta"
     infile = os.path.join(indir, "assembly.fasta")
-    subprocess.call(["busco", "-m", "genome", "-i", infile, "-o", "busco_results", "--out_path", outdir,
-                     "-l", "gammaproteobacteria_odb10", "--cpu", cpus])
+    # subprocess.call(["busco", "-m", "genome", "-i", infile, "-o", "busco_results", "--out_path", outdir,
+    #                 "-l", "gammaproteobacteria_odb10", "--cpu", cpus])
+    proc_busco = subprocess.Popen("busco -m genome -i %s -o busco_results --out_path %s -l gammaproteobacteria_odb10 "
+                                  "--cpu %s"
+                                  % (infile, outdir, cpus), shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc_busco.communicate()
+    strain = indir.split("/")[1]
+    # write logs
+    with open("logs/%s_busco_stderr.log" % strain, "w") as errout:
+        errout.write(err.decode("utf-8"))
+
+    with open("logs/%s_busco_stdout.log" % strain, "w") as stout:
+        stout.write(out.decode("utf-8"))
+
     # run QUAST
     output_dir = os.path.join(outdir, "quast_results")
     subprocess.call(["quast.py", "-t", cpus, "-o", output_dir, infile])
