36da0037f7e2c46be85f80957e57772875a77d71
andrewgull <andrewgull87@gmail.com>
2021-10-01 15:56:58 +0200

    first commit

---
+++ workflow/scripts/join_two_fastas.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Joins Unicycler assembly/prokka annotation and SPAdes plasmid assembly/tRNAScan-SE annotation. """
+import argparse
+import os
+from Bio import SeqIO
+
+
+def get_args():
+    """
+    Get command line arguments
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Joins Unicycler assembly and SPAdes plasmid assembly.\n"
+                    "If no plasmid assembly exists it just copies Unicycler assembly to a new file.",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    parser.add_argument("unicycler_assembly", metavar="<filename>",
+                        help="file with genome assembly produced by Unicycler")
+    parser.add_argument("plasmid_assembly", metavar="<filename>",
+                        help="file with plasmids assembly produced by SPAdes")
+    parser.add_argument("output", metavar="<filename>",
+                        help="new joined assembly file")
+
+    return parser.parse_args()
+
+
+def joiner(file1, file2):
+    """
+    Joins provided files
+    param: file1 - 1st file to join (unicycler assembly or any other fasta)
+    param: file2 - 2nd file to join (plasmid assembly or any other fasta)
+    """
+    # read the 1st file
+    fasta1 = [seq for seq in SeqIO.parse(file1, 'fasta')]
+    # check if plasmid assembly exists and its size
+    if os.path.isfile(file2) and os.path.getsize(file2) > 0:
+        # file exists, read it and join
+        fasta2 = [seq for seq in SeqIO.parse(file2, 'fasta')]
+        fasta_joined = fasta1 + fasta2
+    else:
+        # return the 1st file (unicycler assembly)
+        fasta_joined = fasta1
+        # it will go to a log file
+        print("no finished plasmid assembly file found")
+
+    return fasta_joined
+
+
+if __name__ == '__main__':
+    args = get_args()
+    output_assembly = joiner(file1=args.unicycler_assembly, file2=args.plasmid_assembly)
+    SeqIO.write(output_assembly, args.output, 'fasta')
