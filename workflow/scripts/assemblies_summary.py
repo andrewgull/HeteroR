c8e0bf5ee84ab7b62bed7536bec33bed9416867c
andrewgull <andrewgull87@gmail.com>
2021-10-11 15:53:12 +0200

    add snakemake inputs and output

--- workflow/scripts/assemblies_summary.py
+++ workflow/scripts/assemblies_summary.py
@@ -4,23 +4,47 @@ import os
 from Bio import SeqIO
 
 
-strains = list()
+input_assembly = snakemake.input[0]  # it's a dir path like 'assemblies/DA00000'
+input_plasmid = snakemake.input[1]  # dir name like 'plasmids/DA00000'
+output = snakemake.output[0]
+
+# get strain name
+strain = input_assembly.split('/')
+# names for future table
 col_names = ["Component", "Segments", "Links", "Length", "N50", "Longest_component", "Status"]
-for strain in strains:
-    summary = subprocess.run("sed -n '/^Component/,/^Polishing/{p;/^Polishing/q}' "
-                             "assemblies/%s/unicycler.log | head -n -3" % strain, shell=True, capture_output=True)
-    summary_stdout = summary.stdout.decode("utf-8").splitlines()
-    # i can skip first two elements (header and total): summary_lines[2:len(summary_lines)]
-    summary_lines = [item.split() for item in summary_stdout][2:len(summary_stdout)]
-    summary_df = pd.DataFrame(summary_lines, columns=col_names)
-    summary_df["Strain"] = strain
-    # ideally there should be one chromosomal sequence and the rest should be plasmids
-    summary_df['Type'] = ['Chromosome'] + ['Plasmid'] * (len(summary_df) - 1)
 
-    # get stats from plasmid assemblies: count and length
-    # finished plasmids assembly is called 'scaffolds.fasta'
-    plasmid_assembly = "plasmids/%s/scaffolds.fasta" % strain
-    if os.path.isfile(plasmid_assembly) and os.path.getsize(plasmid_assembly) > 0:
-        plasmid = [len(seq) for seq in SeqIO.parse(plasmid_assembly, 'fasta')]
+# parse unicycler's log file
+summary = subprocess.run("sed -n '/^Component/,/^Polishing/{p;/^Polishing/q}' "
+                         "%s/unicycler.log | head -n -3" % input_assembly, shell=True, capture_output=True)
+summary_stdout = summary.stdout.decode("utf-8").splitlines()
+# i can skip first two elements (header and total): summary_lines[2:len(summary_lines)]
+summary_lines = [item.split() for item in summary_stdout][2:len(summary_stdout)]
+summary_df = pd.DataFrame(summary_lines, columns=col_names)
+summary_df["Strain"] = strain
+# ideally there should be one chromosomal sequence and the rest should be plasmids
+summary_df['Type'] = ['Chromosome'] + ['Plasmid'] * (len(summary_df) - 1)
 
+# get stats from plasmid assemblies: count and length
+# finished plasmids assembly is called 'scaffolds.fasta'
+# create a DataFrame
+plasmid_assembly = "%s/scaffolds.fasta" % input_plasmid
+if os.path.isfile(plasmid_assembly) and os.path.getsize(plasmid_assembly) > 0:
+    plasmid_len = [len(seq) for seq in SeqIO.parse(plasmid_assembly, 'fasta')]
+    plasmid_df = pd.DataFrame({'Length': plasmid_len})
+    plasmid_df['Component'] = None
+    plasmid_df['Segments'] = 1
+    plasmid_df['Links'] = None
+    plasmid_df['N50'] = None
+    plasmid_df['Longest_component'] = plasmid_len
+    plasmid_df['Status'] = 'scaffold'
+    plasmid_df['Strain'] = strain
+    plasmid_df['Type'] = 'Plasmid'
 
+# concatenate summary_df and plasmid_df
+joined_df = pd.concat([summary_df, plasmid_df])
+# rewrite 'Component' to include new plasmids
+joined_df['Component'] = [i for i in range(1, len(joined_df) + 1)]
+
+# write this DataFrame to a file
+# output is declared on the top of he script (and comes from snakemake)
+joined_df.to_csv(path_or_buf=output, sep="\t", index=False)
