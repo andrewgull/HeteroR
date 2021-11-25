81c2b6e10616c394e7a32b0b9c3831f9db97950a
Andrei Guliaev <andrewgull87@gmail.com>
2021-11-25 17:44:19 +0100

    change to iterating through chroms and plasmids

--- workflow/scripts/repeats_in_RGs.py
+++ workflow/scripts/repeats_in_RGs.py
@@ -27,61 +27,56 @@ rgi_notLoose = rgi[rgi["Cut_Off"] != "Loose"]
 with open(in_gff) as f:
     gff = [rec for rec in GFF.parse(f)]
 
-chromosome_genes = [feature for feature in gff[0].features if feature.type == "gene"]  # here we have IDs and positions
-chromosome_id = [gff[0].id]
+# iterate through chromosome and plasmids
+for item in gff:
+    genes = [feature for feature in item.features if feature.type == "gene"]  # here we have IDs and positions
+    item_id = item.id
 
-# and plasmid genes' coordinates and strand
-plasmid_genes = list()
-plasmid_id = list()
-for i in range(1, len(gff)):
-    plasmid_genes.append([feature for feature in gff[i].features if feature.type == "gene"])
-    plasmid_id.append(gff[i].id)
+    # find resistance genes' coords: RGI - resistance, GBK - coords
+    # naive looping approach: 61*4947 comparisons
+    resistance_genes_coords = list()
+    for orf in rgi_notLoose["ORF_ID"]:
+        for gene in genes:
+            if orf.split(" ")[0] in gene.id:
+                resistance_genes_coords.append(gene)
 
-# find resistance genes' coords: RGI - resistance, GBK - coords
-# naive looping approach: 61*4947 comparisons
-resistance_genes_coords = list()
-for orf in rgi_notLoose["ORF_ID"]:
-    for gene in chromosome_genes:
-        if orf.split(" ")[0] in gene.id:
-            resistance_genes_coords.append(gene)
+    print("In record %s %i resistance genes not found" % (item_id, len(rgi_notLoose) - len(resistance_genes_coords)))
 
-print("Coordinates of %i resistance genes not found" % (len(rgi_notLoose) - len(resistance_genes_coords)))
+    # get their coordinates - from GBK
+    # get ± 100 kb region for each gene
 
-# get their coordinates - from GBK
-# get ± 100 kb region for each gene
+    rg_collection = list()
 
-genes_lol = list()
+    for gene in resistance_genes_coords:
+        start = int(gene.location.start)
+        end = int(gene.location.end)
+        span_start = start - span_len
+        # check if it's negative
+        #if span_start < 0:
+        #    span_start = genome_len + span_start
+        span_end = end + span_len
+        row = [gene.id, start, end, span_start, span_end, int(gene.location.strand)]
+        rg_collection.append(row)
 
-for gene in resistance_genes_coords:
-    start = int(gene.location.start)
-    end = int(gene.location.end)
-    span_start = start - span_len
-    # check if it's negative
-    #if span_start < 0:
-    #    span_start = genome_len + span_start
-    span_end = end + span_len
-    row = [gene.id, start, end, span_start, span_end, int(gene.location.strand)]
-    genes_lol.append(row)
+    # a table with span and resistance gene coordinates
+    rg_ranges = pd.DataFrame(columns=["gene_id", "gene_start", "gene_end", "span_start", "span_end", "strand"],
+                             data=rg_collection)
 
-# a table with span and resistance gene coordinates
-rg_spans_and_coords = pd.DataFrame(columns=["gene_id", "gene_start", "gene_end", "span_start", "span_end", "strand"],
-                           data=genes_lol)
+    # turn this data frame to a bed file - but how to deal with negative coordinates?
+    # https://bacteria.ensembl.org/info/website/upload/bed.html
+    # Use bed tools - create a bed file
+    # something should be done with the negative coordinates
+    # split ranges not crossing oriC and not crossing it
+    rg_ranges_pos = rg_ranges[rg_ranges["span_start"] >= 0]
+    rg_ranges_neg = rg_ranges[rg_ranges["span_start"] < 0]
 
-# turn this data frame to a bed file - but how to deal with negative coordinates?
-# https://bacteria.ensembl.org/info/website/upload/bed.html
-# Use bed tools - create a bed file
-# something should be done with the negative coordinates
+    # making a bed file for ranges not crossing oriC
+    bed_file = pd.DataFrame()
 
-rg_spans_and_coords_positive = rg_spans_and_coords[rg_spans_and_coords["span_start"] >= 0]
-rg_spans_and_coords_negative = rg_spans_and_coords[rg_spans_and_coords["span_start"] < 0]
-
-# making a bed file for ranges not crossing oriC
-bed_file = pd.DataFrame()
-
-bed_file["range_start"] = rg_spans_and_coords_positive["span_start"]
-bed_file["range_end"] = rg_spans_and_coords_positive["span_end"]
-bed_file["name"] = rg_spans_and_coords_positive["gene_id"]
-bed_file["score"] = "0"
-bed_file["strand"] = rg_spans_and_coords_positive["strand"]
+    bed_file["range_start"] = rg_ranges_pos["span_start"]
+    bed_file["range_end"] = rg_ranges_pos["span_end"]
+    bed_file["name"] = rg_ranges_pos["gene_id"]
+    bed_file["score"] = "0"
+    bed_file["strand"] = rg_ranges_pos["strand"]
 
 
