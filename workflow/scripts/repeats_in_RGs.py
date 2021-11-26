5b8ba6b79f6238d9cf612685ed73ece9f302bc31
Andrei <andrewgull87@gmail.com>
2021-11-26 11:15:07 +0100

    replace gff loop with a function

--- workflow/scripts/repeats_in_RGs.py
+++ workflow/scripts/repeats_in_RGs.py
@@ -9,38 +9,23 @@ from Bio import SeqIO
 import pandas as pd
 from BCBio import GFF
 
-# cd /home/andrei/Data/HeteroR/test_dir/GRF
-# VARIABLES
-in_rgi = "DA62886_rgi_table.tsv"
-in_gff = "DA62886_genomic.gff"
-genome_len = 5131220  # must be determined for each particular genome
-span_len = 100000
 
-# rgi results - resistance information
-rgi = pd.read_csv(in_rgi, sep="\t")
-# rgi["Cut_Off"].unique()
-# some genes are 'Loose', leave 'Strict' and 'Perfect' only
-rgi_notLoose = rgi[rgi["Cut_Off"] != "Loose"]
-
-# parse GFF annotation file
-# to retrieve chromosomal genes' coordinates and strand
-with open(in_gff) as f:
-    gff = [rec for rec in GFF.parse(f)]
-
-# iterate through chromosome and plasmids
-for item in gff:
-    genes = [feature for feature in item.features if feature.type == "gene"]  # here we have IDs and positions
-    item_id = item.id
+def make_bed_file(gff_record, rgi_dataframe):
+    """
+    makes bed file for genomic ranges with resistance genes
+    """
+    genes = [feature for feature in gff_record.features if feature.type == "gene"]  # here we have IDs and positions
+    item_id = gff_record.id
 
     # find resistance genes' coords: RGI - resistance, GBK - coords
     # naive looping approach: 61*4947 comparisons
     resistance_genes_coords = list()
-    for orf in rgi_notLoose["ORF_ID"]:
+    for orf in rgi_dataframe["ORF_ID"]:
         for gene in genes:
             if orf.split(" ")[0] in gene.id:
                 resistance_genes_coords.append(gene)
 
-    print("In record %s %i resistance genes not found" % (item_id, len(rgi_notLoose) - len(resistance_genes_coords)))
+    message = "In record %s %i resistance genes not found" % (item_id, len(rgi_dataframe) - len(resistance_genes_coords))
 
     # get their coordinates - from GBK
     # get ± 100 kb region for each gene
@@ -77,5 +62,27 @@ for item in gff:
     bed_file["name"] = rg_ranges_pos["gene_id"]
     bed_file["score"] = "0"
     bed_file["strand"] = rg_ranges_pos["strand"]
+    return bed_file, message
 
 
+# cd /home/andrei/Data/HeteroR/test_dir/GRF
+# VARIABLES
+in_rgi = "DA62886_rgi_table.tsv"
+in_gff = "DA62886_genomic.gff"
+genome_len = 5131220  # must be determined for each particular genome
+span_len = 100000
+
+# rgi results - resistance information
+rgi = pd.read_csv(in_rgi, sep="\t")
+# rgi["Cut_Off"].unique()
+# some genes are 'Loose', leave 'Strict' and 'Perfect' only
+rgi_notLoose = rgi[rgi["Cut_Off"] != "Loose"]
+
+# parse GFF annotation file
+# to retrieve chromosomal genes' coordinates and strand
+with open(in_gff) as f:
+    gff = [rec for rec in GFF.parse(f)]
+
+# iterate through chromosome and plasmids
+for item in gff:
+    ranges_bed, bed_message = make_bed_file(item, rgi_notLoose)
