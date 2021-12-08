0bec4dd5edaffc7900e53c76cbf041ad0fce7135
Andrei Guliaev <andrewgull87@gmail.com>
2021-12-08 14:19:34 +0100

    add gff_object(); remove obsolete lines and comments

--- workflow/scripts/GRF_parser.py
+++ workflow/scripts/GRF_parser.py
@@ -30,54 +30,51 @@ def parse_spacer(header):
             repeat_2_end_in_chrom, repeat_len]
 
 
-min_repeat_length = 10  # make a pd DF from headers to filter them effectively?
-with open("/home/andrei/Data/HeteroR/test_dir/GRF/DA62886_perfect_repeats_GRF_test/perfect.spacer.id") as f:
-    spacer_ids = [line.rstrip() for line in f.readlines() if str(min_repeat_length) + "m" not in line]
+def gff_object(features_df, record, strand=1, feature_type="direct_repeat"):
+    """
+    :param features_df: a pd.DataFrame with features for gff object
+    :param record: SeqRecord object from assembly
+    :param strand: strand for GFF files, 1 or -1
+    :param feature_type: name of the feature
+    :return: SeqRecord with features for GFF.write()
+    """
+    # take features found in provided record
+    features_in_record = features_df[features_df.record_id == record.id]
+    # init repeat IDs
+    repeat_id = 0
+    for row in features_in_record.iterrows():
+        repeat_id += 1
+        # use one ID for each pair of repeats (1,2,3,4 elements of each row)
+        record.features.append(SeqFeature(FeatureLocation(ExactPosition(row[1][1]),
+                                                          ExactPosition(row[1][2]), strand=strand),
+                                          type=feature_type, id=str(repeat_id)))
+        record.features.append(SeqFeature(FeatureLocation(ExactPosition(row[1][3]),
+                                                          ExactPosition(row[1][4]), strand=strand),
+                                          type=feature_type, id=str(repeat_id)))
 
+    return record
+
+
+# read spacer IDs
+with open("/home/andrei/Data/HeteroR/test_dir/GRF/DA62886_perfect_repeats_GRF_test/perfect.spacer.id") as f:
+    spacer_ids = [line.rstrip() for line in f.readlines()]
+
+# read assembly
 assembly = [rec for rec in SeqIO.parse("/home/andrei/Data/HeteroR/test_dir/GRF/DA62886_assembly.fasta", "fasta")]
 
-# a line in lines looks like this: >1:0-190543:951:190482:14m'
-# >1:0-190543 - fasta id
-# 951:190482:14m - start of the 1st match, end of the 2nd match and length of the match
-# read GBK to look inside
-gbk = [rec for rec in SeqIO.parse("/home/andrei/Data/HeteroR/test_dir/GRF/DA62886_genomic.gbk", "genbank")]
-
-gbk[0].features[0]
-
-in_rgi = "/home/andrei/Data/HeteroR/test_dir/GRF/DA62886_rgi_table.txt"
-rgi = pd.read_csv(in_rgi, sep="\t")
-
-# parse gff or genebank
-gff = [rec for rec in GFF.parse("/home/andrei/Data/HeteroR/test_dir/GRF/DA62886_genomic.gff")]
-# same structure inside
-gff[0].features[0]
-
-# you have info on genes
-# see the copybook
-# gff files look easier to create
-# each record in assembly should have separate gff file with repeats
-# create a SeqRecord (chromosome or plasmid) where each repeat is a feature with location
-# to get location features parse and calculate coordinates from spacer IDs
-strand = 1  # is always 1
-n = 0
-
-gff_rows = [parse_spacer(line) for line in spacer_ids[:20]]
+# make features rows from spacer IDs
+gff_rows = [parse_spacer(line) for line in spacer_ids]
 gff_df = pd.DataFrame(columns=["record_id", "start_1", "end_1", "start_2", "end_2", "length"], data=gff_rows)
-# TODO: filter this DataFrame by chromosome and lengths
+
+# set minimal repeat length
+min_repeat_length = 15
+# filter out too short repeats
+gff_df = gff_df[gff_df.length > min_repeat_length]
+
+# make a gff object from this filtered data frame
+# one SeqRecord with features per record in assembly
+gff_records = [gff_object(gff_df, record) for record in assembly]
 
 
-for line in gff_df:
-    n += 1
-    repeat_features = parse_spacer(line)
-    # use 1st record from the assembly as an example
-    # create a pair of features for each index
-    assembly[0].features.append(SeqFeature(FeatureLocation(ExactPosition(repeat_features[1]),
-                                                           ExactPosition(repeat_features[2]), strand=strand),
-                                           type='direct_repeat', id=str(n)))
-    assembly[0].features.append(SeqFeature(FeatureLocation(ExactPosition(repeat_features[3]),
-                                                           ExactPosition(repeat_features[4]), strand=strand),
-                                           type='direct_repeat', id=str(n)))
 with open("/home/andrei/Data/HeteroR/test_dir/GRF/DA62886_repeats.gff", 'w') as out_gff:
-    GFF.write(recs=[assembly[0]], out_handle=out_gff, include_fasta=False)
-
-# TODO: expand to all records in assembly
+    GFF.write(recs=gff_records, out_handle=out_gff, include_fasta=False)
