61eabb300f58fc7eeb9c966af79221fa29dcc0bf
Andrei Guliaev <andrewgull87@gmail.com>
2021-12-09 14:52:52 +0100

    first commit

--- workflow/scripts/sync_results.sh
+++ workflow/scripts/sync_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/andrei/Data/HeteroR/results
-directories=( assemblies_joined annotations direct_repeats mapping plasmids qualcheck_assembly qualcheck_reads )
+directories=( assemblies assemblies_joined annotations direct_repeats mapping plasmids qualcheck_assembly qualcheck_reads )
 
 while IFS= read -r line; do
   for dir in "${directories[@]}"; do
