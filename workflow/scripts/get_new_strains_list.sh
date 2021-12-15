d2491988abce7798c656fd0c0c4eb23d8e232e96
Andrei Guliaev <andrewgull87@gmail.com>
2021-12-15 13:57:18 +0100

    add help message

--- workflow/scripts/get_new_strains_list.sh
+++ workflow/scripts/get_new_strains_list.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+print_help() {
+   printf "required arguments:\n sorted list of strains available in resources\n sorted list of strain available on Argos"
+}
+
+if [ "$#" -eq 0 ]
+then
+    print_help
+    exit 1
+fi
+
 # sort lists first
-list_old_sorted=$1
-list_new_sorted=$2
+list_old_sorted=$1  # list of strains currently in ./resources
+list_new_sorted=$2  # list of all strains available on Argos
 
 # update sample list
-comm -13 $1 $2 > strains_to_add.txt
+comm -13 $1 $2 > strains_to_add.txt  # find difference between two lists
 cat list_old_sorted strains_to_add > strains_all.txt
