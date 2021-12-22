0e71cf3fd85ce6fd36dfbbb1933d3e58fd40b68a
Andrei Guliaev <andrewgull87@gmail.com>
2021-12-22 16:13:30 +0100

    update functions for both mismatch and no mismatch grf outputs

--- workflow/scripts/GRF_parser.py
+++ workflow/scripts/GRF_parser.py
@@ -14,12 +14,13 @@ import os
 
 def parse_grf_output(header):
     """
-    :param header: a char string like '>1:0-190543:951:190482:14m'
+    :param header: a char string like '>1:0-190543:951:190482:14m' or '>1:155171-355997:1372:198472:10m1D3m'
     :return: a list of values from parsed header
     """
     first_split = header.split(":")
     record_id = first_split[0][1:]
-    repeat_len = int(first_split[-1][:-1])
+    # this works for both mismatch or no mismatch types of output
+    repeat_len = int(first_split[-1].split("m")[0])
     range_ = first_split[1].split("-")
     range_start, range_end = int(range_[0]), int(range_[-1])
     repeat_1_start_in_range = int(first_split[2])
@@ -60,37 +61,50 @@ def gff_object(features_df, record, strand=1, feature_type="direct_repeat"):
     return record
 
 
+def main(input_assembly, input_grf, min_len):
+    # read spacer IDs
+    with open(input_grf) as f:
+        repeat_ids = [line.rstrip() for line in f.readlines()]
+
+    # read assembly
+    assembly = [rec for rec in SeqIO.parse(input_assembly, "fasta")]
+
+    # make features rows from spacer IDs
+    gff_rows = [parse_grf_output(line) for line in repeat_ids]
+    gff_df = pd.DataFrame(columns=["record_id", "start_1", "end_1", "start_2", "end_2", "length"], data=gff_rows)
+    gff_df.drop_duplicates(inplace=True)
+
+    # filter out too short repeats
+    gff_df = gff_df[gff_df.length > min_len]
+
+    # make a gff object from this filtered data frame
+    # one SeqRecord with features per record in assembly
+    gff_records = [gff_object(gff_df, record) for record in assembly]
+    return gff_records
+
+
 if __name__ == '__main__':
     # DEFINE INPUTS AND OUTPUTS
     # in_spacers = "/home/andrei/Data/HeteroR/test_dir/GRF/DA62886_perfect_repeats_GRF_test/perfect.spacer.id"
     # in_assembly = "/home/andrei/Data/HeteroR/test_dir/GRF/DA62886_assembly.fasta"
     # out_gff = "/home/andrei/Data/HeteroR/test_dir/GRF/DA62886_repeats.gff"
-    grf_out_filename = "perfect.spacer.id"  # IT CAN BE CHANGED
-    in_spacers = os.path.join(snakemake.input[0], grf_out_filename)
+    # grf_out_filename = "perfect.spacer.id"  # IT CAN BE CHANGED
+    in_perfect = os.path.join(snakemake.input[0], "perfect.spacer.id")
+    in_imperfect = os.path.join(snakemake.input[0], "imperfect.id")
     in_assembly = snakemake.input[1]
-    out_gff = snakemake.output[0]
+    out_gff_perfect = snakemake.output[0]
+    out_gff_imperfect = snakemake.output[1]
     # set minimal repeat length
     min_repeat_length = int(snakemake.params[0])
 
-    # read spacer IDs
-    with open(in_spacers) as f:
-        spacer_ids = [line.rstrip() for line in f.readlines()]
-
-    # read assembly
-    assembly = [rec for rec in SeqIO.parse(in_assembly, "fasta")]
-
-    # make features rows from spacer IDs
-    gff_rows = [parse_spacer(line) for line in spacer_ids]
-    gff_df = pd.DataFrame(columns=["record_id", "start_1", "end_1", "start_2", "end_2", "length"], data=gff_rows)
-    gff_df.drop_duplicates(inplace=True)
-
-    # filter out too short repeats
-    gff_df = gff_df[gff_df.length > min_repeat_length]
-
-    # make a gff object from this filtered data frame
-    # one SeqRecord with features per record in assembly
-    gff_records = [gff_object(gff_df, record) for record in assembly]
-
+    # create gff records from perfect.spacer.id
+    gff_records_perfect = main(in_assembly, in_perfect, min_len=min_repeat_length)
     # write to a GFF file
-    with open(out_gff, 'w') as out:
-        GFF.write(recs=gff_records, out_handle=out, include_fasta=False)
+    with open(out_gff_perfect, 'w') as out:
+        GFF.write(recs=gff_records_perfect, out_handle=out, include_fasta=False)
+
+    # create gff records from imperfect.id
+    gff_records_imperfect = main(in_assembly, in_imperfect, min_len=min_repeat_length)
+    # write them to a file
+    with open(out_gff_imperfect, 'w') as out:
+        GFF.write(recs=gff_records_imperfect, out_handle=out, include_fasta=False)
