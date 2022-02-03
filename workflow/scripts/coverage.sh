31515f59ff412a804763d4ce21d2dea7bb0d1a81
Andrei <andrewgull87@gmail.com>
2022-02-03 16:31:01 +0100

    update path to data_raw

--- workflow/scripts/coverage.sh
+++ workflow/scripts/coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MACHINE=$1
 GENOME_SIZE="5131220"
-for strain in data_raw/DA*; do
+for strain in resources/data_raw/DA*; do
   # get total length
   L=`seqkit stats $strain/$MACHINE/*.gz -T | cut -f 5 | sed '1d' | awk '{s+=$1}END{print s}'`
   # get mean coverage
