6f91376447d8b4830a82eac18e411097ffd09fd1
Andrei <andrewgull87@gmail.com>
2022-02-14 12:06:42 +0100

    update

--- workflow/scripts/trim_nanopore.py
+++ workflow/scripts/trim_nanopore.py
@@ -6,6 +6,8 @@ trim Nanopore reads if probability is higher than a threshold
 
 import math
 import subprocess
+import shutil
+import os
 
 
 def do_not_trim():
@@ -17,9 +19,9 @@ def do_trim():
     return 0
 
 
-# inputs and parameters
-input_file = snakemake.input[0]
-output_file = snakemake.output[0]
+# inputs
+input_file = snakemake.input[0]  # "resources/data_raw/{strain}/Nanopore/{strain}_all.fastq.gz"
+output_file = snakemake.output[0]  # "results/data_filtered/{strain}/Nanopore/{strain}_all.fastq.gz"
 # coverage and probability params
 genome_len = snakemake.params[0]  # 5131220
 intercept = snakemake.params[1]  # 2.2401998
@@ -32,19 +34,39 @@ percent = snakemake.parmas[6]
 bases = snakemake.params[7]
 threads = snakemake.params[8]
 
-proc = subprocess.Popen("seqkit stats %s -T | cut -f5 | tail -n 1" % input_file, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err = proc.communicate()
-sum_len = int(out.decode("utf-8"))
+# calculate the coverage
+proc = subprocess.Popen("seqkit stats %s -T | cut -f5 | tail -n 1" % input_file, shell=True, stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+out_cov, err_cov = proc.communicate()
+sum_len = int(out_cov.decode("utf-8"))
 coverage = sum_len / genome_len
 # here comes the smart formula from the logistic regression model
 odds = math.exp(intercept + coefficient * coverage)
 # probability of being incomplete after trimming
 probability = odds / (1 + odds)
 
+# create output directories if they do not exits
+strain = output_file.split("/")[2]
+output_directory = "/".join(output_file.split("/")[:-1])  # 'results/data_filtered/{strain}/Nanopore'
+
+
+
 if probability >= cut_off:
     # very likely the assembly will be incomplete
     do_not_trim()
+    # just copy the file to results
+    # create a directory first
+    if not os.path.exists(output_directory):
+        os.mkdir("results/data_filtered/%s/Nanopore" % strain)
+    else:
+
+    shutil.copy(input_file, output_file)
+
 else:
     do_trim()
-    subprocess.Popen("filtlong --min_length {params.min_len} --length_weight {params.len_weight} --keep_percent {params.perc} --target_bases {params.bases} {input} 2> {log} | pigz -c -p {threads} > {output}")
-
+    # here piping may not work well, split in two commands if so
+    subprocess.Popen("filtlong --min_length %i --length_weight %i --keep_percent %i --target_bases %i %s "
+                     "| pigz -c -p %i > %s" % (min_len, len_weight, percent, bases, input_file, threads, output_file),
+                     shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # both go to the snakemake's log
+    out_filter, err_filter = proc.communicate()
