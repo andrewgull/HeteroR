685465db2e4e754f390dd826c7c03208eb6535ad
Andrei <andrewgull87@gmail.com>
2022-02-14 13:31:40 +0100

    update

--- workflow/scripts/trim_nanopore.py
+++ workflow/scripts/trim_nanopore.py
@@ -10,15 +10,6 @@ import shutil
 import os
 
 
-def do_not_trim():
-    return 0
-
-
-def do_trim():
-    # run filtlong
-    return 0
-
-
 # inputs
 input_file = snakemake.input[0]  # "resources/data_raw/{strain}/Nanopore/{strain}_all.fastq.gz"
 output_file = snakemake.output[0]  # "results/data_filtered/{strain}/Nanopore/{strain}_all.fastq.gz"
@@ -34,10 +25,15 @@ percent = snakemake.parmas[6]
 bases = snakemake.params[7]
 threads = snakemake.params[8]
 
+log_messages = list()
+
 # calculate the coverage
 proc = subprocess.Popen("seqkit stats %s -T | cut -f5 | tail -n 1" % input_file, shell=True, stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE)
 out_cov, err_cov = proc.communicate()
+log_messages.append(out_cov)
+log_messages.append(err_cov)
+
 sum_len = int(out_cov.decode("utf-8"))
 coverage = sum_len / genome_len
 # here comes the smart formula from the logistic regression model
@@ -48,25 +44,30 @@ probability = odds / (1 + odds)
 # create output directories if they do not exits
 strain = output_file.split("/")[2]
 output_directory = "/".join(output_file.split("/")[:-1])  # 'results/data_filtered/{strain}/Nanopore'
-
-
+# 'data_filtered' may not exist, then create 'data_filtered/strain' and 'data_filtered/strainNanopore'
+# in a sequential way because this is how os.mkdir works
+if not os.path.exists("results/data_filtered"):
+    os.mkdir("results/data_filtered")
+    os.mkdir("results/data_filtered/%s" % strain)
+    os.mkdir("results/data_filtered/%s/Nanopore" % strain)
+# now the full path exists
 
 if probability >= cut_off:
     # very likely the assembly will be incomplete
-    do_not_trim()
     # just copy the file to results
-    # create a directory first
-    if not os.path.exists(output_directory):
-        os.mkdir("results/data_filtered/%s/Nanopore" % strain)
-    else:
-
     shutil.copy(input_file, output_file)
-
 else:
-    do_trim()
+    # output file directory should have been created
     # here piping may not work well, split in two commands if so
     subprocess.Popen("filtlong --min_length %i --length_weight %i --keep_percent %i --target_bases %i %s "
                      "| pigz -c -p %i > %s" % (min_len, len_weight, percent, bases, input_file, threads, output_file),
                      shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     # both go to the snakemake's log
     out_filter, err_filter = proc.communicate()
+    log_messages.append(out_filter)
+    log_messages.append(err_filter)
+
+# write log files
+with open(snakemake.log[0], "w") as log_out:
+    for message in log_messages:
+        log_out.write(str(message) + "\n")
