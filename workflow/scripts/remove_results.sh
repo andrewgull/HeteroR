c82681f5434cef6dba6b49d2e75708bb2c902e48
Andrei Guliaev <andrewgull87@gmail.com>
2022-02-15 12:10:45 +0100

    finish

--- workflow/scripts/remove_results.sh
+++ workflow/scripts/remove_results.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 cd /home/andrei/Data/HeteroR/results
 
-directories=( assemblies assemblies_joined annotations direct_repeats mapping plasmids qualcheck_assembly qualcheck_reads resistance_genes )
+directories=( annotations assemblies assemblies_joined data_filtered direct_repeats mapping plasmids qualcheck_assembly qualcheck_reads resistance_genes )
 
 print_help() {
    echo "this script removes directories in 'results' for a particular strain"
    echo "required argument: list of strains, one per line"
 }
+
+
+if [ "$#" -eq 0 ]
+then
+    print_help
+    exit 1
+fi
+
+# 'line' is 'strain'
+while IFS= read -r line; do
+  for dir in "${directories[@]}"; do
+    #echo "$dir/$line"
+    rm -r "$dir/$line"
+  done
+  # rm final
+  #echo "final/$line""_all.done"
+  rm "final/$line""_all.done"
+done < "$1"
