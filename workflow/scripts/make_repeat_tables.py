53b24c8a085e1a877acbdf9bfdc34801375008db
Andrei <andrewgull87@gmail.com>
2022-03-18 17:56:13 +0100

    fix inputs rule dr2csv and make_repeat_tables.py

--- workflow/scripts/make_repeat_tables.py
+++ workflow/scripts/make_repeat_tables.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("/home/andrei/GitProjects/HeteroR/workflow/scripts")
 from GRF_parser import parse_grf_output_no_ranges
 import pandas as pd
+import os
 
 
 def make_repeats_df(spacer_file, strain_index=0):
@@ -16,6 +17,7 @@ def make_repeats_df(spacer_file, strain_index=0):
 
 
 # snakemake.input[0] looks like "results/direct_repeats/{strain}/repeats_no_mismatch/perfect.spacer.id"
-repeat_df = make_repeats_df(snakemake.input[0], strain_index=2)
+in_file = os.path.join(snakemake.input[0], "perfect.spacer.id")
+repeat_df = make_repeats_df(in_file, strain_index=2)
 repeat_df.drop_duplicates(inplace=True)
 repeat_df.to_csv(snakemake.output[0])
