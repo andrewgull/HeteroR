82fe55a8759f3a042190b30055ac7add9dc10144
Andrei <andrewgull87@gmail.com>
2022-03-31 17:04:11 +0200

    first commit

---
+++ workflow/scripts/filter_repeat_tables.R
@@ -0,0 +1,17 @@
+# script to filter out repeat pairs that do not span a region's center
+
+filter_center <- function(bed_file, repeat_table, output_table){
+  # function to filter out repeat pairs not spanning a region's center
+  bed_df <- read.delim(bed_file, header=FALSE)
+  bed_df$gene_center <- round((bed_df$V3 - bed_df$V2 + 1)/2)
+  bed_df <- rename(bed_df, "record_id"=V4)
+  bed_df <- select(bed_df, record_id, gene_center)
+  # read {strain}_repeats.csv
+  repeat_df <- read.csv(repeat_table, header=TRUE)
+  repeat_df <- full_join(bed_df, repeat_df, by="record_id")
+  repeat_df$spans_center <- if_else(repeat_df$end_1 <= repeat_df$gene_center & repeat_df$start_2 >= repeat_df$gene_center, "yes", "no")
+  repeat_df_center <- filter(repeat_df, spans_center == "yes")
+  write.table(x=repeat_df_center, file=output_table, sep="\t", row.names = FALSE)
+}
+
+filter_center(snakemake@input[[1]], snakemake@input[[2]], snakemake@output[[1]])
