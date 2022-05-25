ad963ae9fdae5ccae1d9e74cce19bc64a05ea6aa
Andrei Guliaev <andrewgull87@gmail.com>
2022-05-25 15:10:46 +0200

    update: automatic retrieval of strains from resources, use stdout

--- workflow/scripts/get_new_strains_list.sh
+++ workflow/scripts/get_new_strains_list.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 print_help() {
-   printf "required arguments:\n sorted list of strains available in resources\n sorted list of strain available on Argos"
+   printf "This script takes strains available in ./resources,
+compares them to a provided list of strains
+and returns strains in the list but not in resources
+Required arguments:
+    list of strain available on Argos\n"
 }
 
 if [ "$#" -eq 0 ]
@@ -10,10 +14,17 @@ then
     exit 1
 fi
 
-# sort lists first
-list_old_sorted=$1  # list of strains currently in ./resources
-list_new_sorted=$2  # list of all strains available on Argos
+## VARIABLES ##
+RESOURCES_PATH="/home/andrei/Data/HeteroR/resources/data_raw"
+STRAINS_ALL_ARGOS=$1  # list of all strains available on Argos
 
-# update sample list
-comm -13 $1 $2 > strains_to_add.txt  # find difference between two lists
-cat list_old_sorted strains_to_add > strains_all.txt
+# GET SORTED LIST OF STRAINS FROM RESOURCES
+ls $RESOURCES_PATH | sort > strains_in_resources.tmp
+
+# SORT PROVIDED STRAINS LIST
+cat $STRAINS_ALL_ARGOS | sort > strains_on_argos.tmp
+
+# FIND DIFFERENCE BETWEEN TWO LISTS
+comm -13 strains_in_resources.tmp strains_on_argos.tmp
+rm strains_in_resources.tmp
+rm strains_on_argos.tmp
