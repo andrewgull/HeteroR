d864e68efbba5dbb3d48adbdf3ad9a04781d3c46
Andrei Guliaev <andrewgull87@gmail.com>
2022-06-14 16:23:38 +0200

    working version (repeat summary)

--- workflow/scripts/make_repeat_summary_table.py
+++ workflow/scripts/make_repeat_summary_table.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # script to join repeat summary tables and calculate AR length and span center
 import pandas as pd
 import numpy as np
@@ -5,38 +6,41 @@ import numpy as np
 
 # repeat_files_lst = ["/home/andrei/Data/HeteroR/results/annotations/DA62886/repeats/DA62886_repeats.csv",
 #                    "/home/andrei/Data/HeteroR/results/annotations/DA62888/repeats/DA62888_repeats.csv"]
-repeat_files_lst = snakemake.input[0]
-bed_files_lst = snakemake.input[1]
 
-with open(snakemake.log[0], "w") as log:
-    # read csv files with repeat pairs and join them into a DataFrame
-    repeat_df_list = [pd.read_csv(f) for f in repeat_files_lst]
-    repeat_df = pd.concat(repeat_df_list)
+last_file = int(len(snakemake.input)/2)  # index of the first file in the bed input
+repeat_files_lst = [snakemake.input[i] for i in range(last_file)]  # doesn't include last_file
+bed_files_lst = [snakemake.input[i] for i in range(last_file, len(snakemake.input))]  # does include last file
 
-    # calculate Amplifiable Region length
-    repeat_df["AR_length"] = repeat_df.start_2 - repeat_df.end_1 + 1
+# read csv files with repeat pairs and join them into a DataFrame
+repeat_df_list = [pd.read_csv(f) for f in repeat_files_lst]
+repeat_df = pd.concat(repeat_df_list)
 
-    # bed_files_lst = ["/home/andrei/Data/HeteroR/results/direct_repeats/DA62886/regions/regions_within.bed",
-    #                 "/home/andrei/Data/HeteroR/results/direct_repeats/DA62888/regions/regions_within.bed"]
+# calculate Amplifiable Region length
+repeat_df["AR_length"] = repeat_df.start_2 - repeat_df.end_1 + 1
 
-    # read bed files and join them into a DataFrame
-    bed_df_list = [pd.read_csv(f, delimiter="\t", header=None) for f in bed_files_lst]
-    bed_df = pd.concat(bed_df_list)
+# bed_files_lst = ["/home/andrei/Data/HeteroR/results/direct_repeats/DA62886/regions/regions_within.bed",
+#                 "/home/andrei/Data/HeteroR/results/direct_repeats/DA62888/regions/regions_within.bed"]
 
-    # rename V4 (4th col) to 'record_id'
-    bed_df = bed_df.rename(columns={3: "record_id"})
-    # calculate gene center location
-    bed_df["gene_center"] = (bed_df[2] - bed_df[1] + 1)/2
-    # select only required columns
-    bed_df = bed_df[["record_id", "gene_center"]]
+# read bed files and join them into a DataFrame
+bed_df_list = [pd.read_csv(f, delimiter="\t", header=None) for f in bed_files_lst]
+bed_df = pd.concat(bed_df_list)
 
-    # join repeats and bed data frames
-    # ow=outer - use union of keys from both data frames, analogous to inner join that I used in dplyr
-    repeat_df_merged = pd.merge(repeat_df, bed_df, on="record_id", how="outer")
+# rename V4 (4th col) to 'record_id'
+bed_df = bed_df.rename(columns={3: "record_id"})
+# calculate gene center location
+bed_df["gene_center"] = (bed_df[2] - bed_df[1] + 1)/2
+# select only required columns
+bed_df = bed_df[["record_id", "gene_center"]]
 
-    # make spans_center column
-    repeat_df_merged["spans_center"] = np.where((repeat_df_merged['end_1'] <= repeat_df_merged["gene_center"]) &
-                                                (repeat_df_merged["start_2"] >= repeat_df_merged["gene_center"]), 'yes', 'no')
+# join repeats and bed data frames
+# ow=outer - use union of keys from both data frames, analogous to inner join that I used in dplyr
+repeat_df_merged = pd.merge(repeat_df, bed_df, on="record_id", how="outer")
 
-    # write to output
-    repeat_df_merged.to_csv(snakemake.output[0])
+# make spans_center column
+repeat_df_merged["spans_center"] = np.where((repeat_df_merged['end_1'] <= repeat_df_merged["gene_center"]) &
+                                            (repeat_df_merged["start_2"] >= repeat_df_merged["gene_center"]), 'yes', 'no')
+# drop the first column
+repeat_df_merged.drop("Unnamed: 0", axis=1, inplace=True)
+
+# write to output
+repeat_df_merged.to_csv(snakemake.output[0])
