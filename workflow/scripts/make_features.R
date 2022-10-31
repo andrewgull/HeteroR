4f63f8af91c759c50003d86db49c4660a6f2442f
Andrei <andrewgull87@gmail.com>
2022-10-31 17:17:50 +0100

    Update make_features.R

--- workflow/scripts/make_features.R
+++ workflow/scripts/make_features.R
@@ -43,7 +43,10 @@ if (is.null(opt$table)) {
 # SHORTCUTS FOR INPUTS/OUTPUTS
 bed_file_paths <- opt$bed # character vector from expand()
 repeat_csv_paths <- opt$table # character vector from expand()
+rgi_csv_paths <- opt$resistance_genes # character vector from expand()
 output_file <- opt$out # a tsv file
+rgi_identity <- 90
+rgi_length_percent <- 90
 
 ###################
 ### READ INPUTS ###
@@ -64,30 +67,24 @@ bed_df <- bind_rows(lapply(bed_file_paths, function(x) {
 }))
 
 # READ RGI DATA
-
-rgi <- bind_rows(lapply(strains, function(x){
-  df <- read.delim(paste0("/home/andrei/Data/HeteroR/results/resistance_genes/", x, "/rgi_table.txt"), na.strings = "n/a")
-  df$strain <- x
+rgi <- bind_rows(lapply(rgi_csv_paths, function(x) {
+  df <- read_delim(x, na.strings = "n/a")
+  # parse strain name from input path
+  df$strain <- "strain"
   return(df)
-}) )
+}))
 
+# GET AB GROUPS
 ab_groups <- unique(rgi$Drug.Class)
 
-# filter out Loose hits and some columns
-rgi <- rgi %>% 
-  filter( Cut_Off != "Loose" ) %>% 
-  select(-c(Predicted_Protein, CARD_Protein_Sequence, Contig, Start, Stop, Orientation, Predicted_DNA, Note))
+#############################
+### SOME INPUT PROCESSING ###
+#############################
 
-# turn into a tibble
-rgi <- as_tibble(rgi)
+# calculate AR length
+repeat_df$AR_length <- repeat_df$start_2 - repeat_df$end_1 + 1
 
-# add record id and put it in front of the table
-rgi$record_id <- map_chr(rgi$ORF_ID, function(x) strsplit(x, " ")[[1]][1])
-rgi <- relocate(rgi, strain, record_id, .before=ORF_ID)
-
-rgi
-
-# find gene center
+# find gene center in bed files
 bed_df$gene_center <- round((bed_df$V3 - bed_df$V2 + 1) / 2)
 bed_df <- rename(bed_df, "record_id" = V4)
 bed_df <- select(bed_df, strain, record_id, gene_center)
@@ -95,10 +92,7 @@ bed_df <- select(bed_df, strain, record_id, gene_center)
 # bed_df contains slightly more genes than repeat_df (+624)
 # BUT repeat_df is important, that's why I use left_join() below
 
-# calculate AR length
-repeat_df$AR_length <- repeat_df$start_2 - repeat_df$end_1 + 1
-
-# join the two tables
+# join BED and Repeats
 repeat_df <- left_join(repeat_df, bed_df, by = "record_id")
 repeat_df$spans_center <- if_else(
     repeat_df$end_1 <= repeat_df$gene_center &
@@ -111,9 +105,46 @@ repeat_df <- repeat_df %>%
   rename("strain" = strain.x) %>%
   relocate(strain, .before = record_id)
 
-# copy for features
-# and filter strains you want to exclude
+# RGI filtering
+# add record id and put it in front of the table
+rgi$record_id <- map_chr(rgi$ORF_ID, function(x) strsplit(x, " ")[[1]][1])
+rgi <- relocate(rgi, strain, record_id, .before = ORF_ID)
+
+# filter out Loose hits and some columns
+rgi <- rgi %>%
+  filter(Cut_Off != "Loose") %>%
+  select(-c(Predicted_Protein, CARD_Protein_Sequence,
+            Contig, Start, Stop, Orientation, Predicted_DNA, Note))
+
+# filter by Identity
+rgi <- rgi %>% filter(Best_Identities >= rgi_identity)
+
+# filter by Percentage Length
+rgi <- filter(rgi, Percentage.Length.of.Reference.Sequence >= rgi_length_percent) # nolint
+
+# filter by Resistance Mechanism
+stay <- c("antibiotic target replacement", "antibiotic target protection", "antibiotic inactivation") # nolint
+rgi <- filter(rgi, Resistance.Mechanism %in% stay)
+
+# filter by SNP
+rgi <- filter(rgi, is.na(SNPs_in_Best_Hit_ARO))
+
+##############################
+### MAKE BASIC FEATURES DF ###
+##############################
 features <- repeat_df %>%
   select(-c(gene_center, start_1, end_1, start_2, end_2, X))
 
+# join it with filtered RGI
+features <- full_join(features, rgi, by = c("strain", "record_id"))
 
+# remove some columns
+features <- features %>%
+  select(-c(ARO, Cut_Off, Pass_Bitscore, Best_Hit_Bitscore, Best_Hit_ARO, 
+            Best_Identities, Model_ID, Model_type, Nudged, SNPs_in_Best_Hit_ARO,
+            Other_SNPs, ID, Percentage.Length.of.Reference.Sequence))
+
+# filter genes without Resistance.Mechanism
+features <- filter(features, !is.na(Resistance.Mechanism))
+
+# read testing results
