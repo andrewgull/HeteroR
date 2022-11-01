762ec545cc2653a4cac419350a318fe25e3550df
Andrei <andrewgull87@gmail.com>
2022-11-01 16:08:50 +0100

    Update make_features.R
    update

--- workflow/scripts/make_features.R
+++ workflow/scripts/make_features.R
@@ -11,15 +11,25 @@ option_list <- list(
                default = NULL,
                help = "A path to BED files",
                metavar = "character"),
-   make_option(c("-t", "--table"),
+   make_option(c("-r", "--repeat_tables"),
                type = "character",
                default = NULL,
                help = "A path to tables with repeat pairs (csv)",
                metavar = "character"),
-   make_option(c("-r", "--resistance_genes"),
+   make_option(c("-g", "--resistance_genes"),
                type = "character",
                default = NULL,
                help = "A path to tables with resistance genes (csv)",
+               metavar = "character"),
+   make_option(c("-t", "--resistance_testing"),
+               type = "character",
+               default = NULL,
+               help = "A path to tables with resistance testing results (csv)",
+               metavar = "character"),
+   make_option(c("-a", "--assembly_summaries"),
+               type = "character",
+               default = NULL,
+               help = "A path to assemblies summaries tables (tsv)",
                metavar = "character"),
    make_option(c("-o", "--out"),
                 type = "character",
@@ -44,9 +54,12 @@ if (is.null(opt$table)) {
 bed_file_paths <- opt$bed # character vector from expand()
 repeat_csv_paths <- opt$table # character vector from expand()
 rgi_csv_paths <- opt$resistance_genes # character vector from expand()
+testing_csv <- opt$resistance_testing # one file
+assembly_summaries <- opt$assembly_summaries # character vector from expand()
 output_file <- opt$out # a tsv file
 rgi_identity <- 90
 rgi_length_percent <- 90
+target_rg <- "beta-lactamase"
 
 ###################
 ### READ INPUTS ###
@@ -77,9 +90,46 @@ rgi <- bind_rows(lapply(rgi_csv_paths, function(x) {
 # GET AB GROUPS
 ab_groups <- unique(rgi$Drug.Class)
 
+# READ LAB TESTING RESULTS
+resistance_df <- read_csv(testing_csv)
+resistance_df$resistance <- as.factor(resistance_df$resistance)
+
+# READ ASSEMBLY SUMMARIES
+assembly_summary <- lapply(assembly_summaries, function(x) {
+  strain <- "strain function here"
+  df <- read_delim(x)
+  df$Links <- as.integer(gsub(",", "", df$Links, fixed = TRUE))
+  df$Length <- as.integer(gsub(",", "", df$Length, fixed = TRUE))
+  df$N50 <- as.integer(gsub(",", "", df$N50, fixed = TRUE))
+  df$Longest_component <- as.integer(gsub(",", "", df$Longest_component, fixed = TRUE)) # nolint
+  return(df)
+})
+
+# READ GFF FILES
+# a function to read GFF files using STANDARD read.delim
+read_gff <- function(gff_filename){
+  # read and process
+  df <- as_tibble(read.delim(gff_filename, header=F, comment.char="#")) %>%
+  select(V1, V3, V4, V5, V7, V9) %>% 
+  filter(V3=="gene")
+  return(df)
+  }
+
+
 #############################
 ### SOME INPUT PROCESSING ###
 #############################
+
+# bind assembly summaries to a single file
+assembly_summary <- bind_rows(assembly_summary)
+
+# calculate plasmid counts
+plasmid_counts <- assembly_summary %>%
+  group_by(Strain, Type) %>%
+  summarise(n.plasmids = n()) %>%
+  filter(Type == "Plasmid") %>%
+  select(-Type) %>%
+  rename("strain" = Strain)
 
 # calculate AR length
 repeat_df$AR_length <- repeat_df$start_2 - repeat_df$end_1 + 1
@@ -147,4 +197,46 @@ features <- features %>%
 # filter genes without Resistance.Mechanism
 features <- filter(features, !is.na(Resistance.Mechanism))
 
-# read testing results
+# join features and testing results
+features <- left_join(features, resistance_df, by = "strain")
+
+# join features and plasmid counts
+features <- left_join(features, plasmid_counts, by = "strain")
+# NA in n.plasmids means 0
+features$n.plasmids <- ifelse(is.na(features$n.plasmids), 0, features$n.plasmids) # nolint
+features <- features %>% relocate(n.plasmids, .before = "ORF_ID")
+
+##########################
+### BL LABELS & COUNTS ###
+##########################
+
+# label beta-lactamases (or other RGs of interest)
+f_bl_filt <- features %>%
+  select(strain, record_id, AMR.Gene.Family) %>%
+  distinct() %>%
+  mutate(is_beta_lac = grepl(target_rg, AMR.Gene.Family))
+# if mutate() doesn't work make it on a separate line
+
+# count BL genes (or other type)
+n_beta_lac <- f_bl_filt %>%
+  group_by(strain) %>%
+  summarise(n.beta.lac = sum(is_beta_lac))
+
+# add is.beta.lac to features for future calculations
+features <- left_join(features, f_bl_filt %>%
+  select(record_id, is_beta_lac), by = "record_id") %>%
+    relocate(is_beta_lac, .before = "ORF_ID")
+
+###########################
+### FEATURES PER STRAIN ###
+###########################
+features_strain <- features %>%
+  select(strain, resistance, n.plasmids) %>%
+  distinct()
+
+# join BL counts with features_strain
+features_strain <- left_join(features_strain, n_beta_lac, by = "strain")
+
+###########################
+### LOCATION ON PLASMID ###
+###########################
