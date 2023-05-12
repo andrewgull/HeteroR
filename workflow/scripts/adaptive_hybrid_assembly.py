94daf19d83d368ab90de0bdbd59970a4cf325d82
andrewgull <andrewgull87@gmail.com>
2023-05-12 13:55:36 +0200

    adding f-strings and constants
    replace regular formatted strings with f-strings;
    capitalize names of constants;
    as was suggested by pylint

--- workflow/scripts/adaptive_hybrid_assembly.py
+++ workflow/scripts/adaptive_hybrid_assembly.py
@@ -5,124 +5,115 @@ import subprocess
 import os
 
 # DEFINE INPUTS/OUTPUTS
-short_reads_1 = snakemake.input[0]
-short_reads_2 = snakemake.input[1]
-long_reads = snakemake.input[2]
-assembly_dir = snakemake.output[0]
-draft_dir = snakemake.output[1]
-polish_dir = snakemake.output[2]
+SHORT_READS_1 = snakemake.input[0]
+SHORT_READS_2 = snakemake.input[1]
+LONG_READS = snakemake.input[2]
+ASSEMBLY_DIR = snakemake.output[0]
+DRAFT_DIR = snakemake.output[1]
+POLISH_DIR = snakemake.output[2]
 
 # DEFINE PARAMS
-threads = snakemake.threads
-basecaller = snakemake.params[0]
-genome_size = snakemake.params[1]
-coverage = snakemake.params[2]   # coverage for Flye
-genome_length = snakemake.params[3]  # expected genome length
-cov_threshold = snakemake.params[4]  # threshold value to choose assembler, < 20x is good for Uni
+THREADS = snakemake.threads
+BASECALLER = snakemake.params[0]
+GENOME_SIZE = snakemake.params[1]
+COVERAGE = snakemake.params[2]   # coverage for Flye
+GENOME_LENGTH = snakemake.params[3]  # expected genome length
+COV_THRESHOLD = snakemake.params[4]  # threshold value to choose assembler, < 20x is good for Uni
 
 # LIST TO KEEP STDOUT/STDERR
-outs = list()
+outs = []
 
 # OPEN LOG
 with open(snakemake.log[0], "w") as f:
     sys.stderr = sys.stdout = f
     
-    # CALCULATE COVERAGE
-    seqkit_out = subprocess.run("seqkit stats %s -T" % long_reads, shell=True, capture_output=True, text=True)
-    gen_coverage = int(seqkit_out.stdout.split("\n")[1].split("\t")[4])/genome_length
+    # CALCULATE COVERAGE in order to decide which assembler to use
+    seqkit_out = subprocess.run(f"seqkit stats {LONG_READS} -T", shell=True, capture_output=True, text=True, check=True)
+    gen_coverage = int(seqkit_out.stdout.split("\n")[1].split("\t")[4])/GENOME_LENGTH
 
-    # SET UNI STATUS
-    unicycler_status = "ok"
+    # SET UNICYCLER STATUS, it is needed to decide whether to ditch Unicycler if it crashes
+    UNICYCLER_STATUS = "ok"
 
-    # SPARSE COV IS FOR UNI ELSE USE FMP
-    if gen_coverage <= cov_threshold:
+    # RUN UNICYCLER IF COVERAGE IS SHALLOW
+    if gen_coverage <= COV_THRESHOLD:
         # RUN UNICYCLER
-        unicycler_out = subprocess.run("unicycler -1 %s -2 %s -l %s -t %i -o %s "
-                                   % (short_reads_1, short_reads_2, long_reads, threads, assembly_dir),
-                                   shell=True, capture_output=True, text=True)
-        # ADD CAPTURED OUT TO OUTS
+        unicycler_out = subprocess.run(f"unicycler -1 {SHORT_READS_1} -2 {SHORT_READS_2} -l {LONG_READS} -t {THREADS} -o {ASSEMBLY_DIR}",
+                                   shell=True, capture_output=True, text=True, check=True)
+        # ADD CAPTURED OUTPUT TO OUTS
         outs.append(unicycler_out)
 
-        # CHECK ERRORS
+        # CHECK FOR ERRORS
         if len(unicycler_out.stderr) > 0:
-            unicycler_status = "error"
+            UNICYCLER_STATUS = "error"
             print("The genome coverage is sparse, but Unicycler has crashed.")
         else:
             # all good, no need to run FMP
             # create dirs: polished, drafts
-            os.mkdir(draft_dir)
-            os.mkdir(polish_dir)
+            os.mkdir(DRAFT_DIR)
+            os.mkdir(POLISH_DIR)
             print("The genome coverage is sparse, Unicycler has been chosen.\nEmpty draft and polish dirs have been created.")
     
-    # RUN FMP IF UNI BROKE OR COV IS SPARSE
-    elif gen_coverage > cov_threshold or unicycler_status == "error":     
-        # MK ASSEMBLY DIR
-        # it can exist after previous runs of the pipeline
-        if not os.path.exists(assembly_dir):
-            os.mkdir(assembly_dir)
+    # RUN FMP IF UNICYCLER BROKE OR COVERAGE IS SPARSE
+    elif gen_coverage > COV_THRESHOLD or UNICYCLER_STATUS == "error":
+        # MK ASSEMBLY DIR but it can exist after previous runs of the pipeline
+        if not os.path.exists(ASSEMBLY_DIR):
+            os.mkdir(ASSEMBLY_DIR)
         
         # RUN FLYE
         print("Flye-Medaka-Polypolish will be chosen to assemble the genome")
-        flye_out = subprocess.run("flye --nano-raw %s --threads %i --out-dir %s -g %s --asm-coverage %i" %
-                                  (long_reads, threads, assembly_dir, genome_size, coverage),
-                                  shell=True, capture_output=True, text=True)
+        flye_out = subprocess.run(f"flye --nano-raw {LONG_READS} --threads {THREADS} --out-dir {ASSEMBLY_DIR} -g {GENOME_SIZE} --asm-coverage {COVERAGE}",
+                                  shell=True, capture_output=True, text=True, check=True)
         outs.append(flye_out)
 
         # RUN MEDAKA
-        medaka_out = subprocess.run("medaka_consensus -i %s -d %s/assembly.fasta -o %s -t %i -m %s" %
-                                    (long_reads, assembly_dir, draft_dir, threads, basecaller),
-                                    shell=True, capture_output=True, text=True)
+        medaka_out = subprocess.run(f"medaka_consensus -i {LONG_READS} -d {ASSEMBLY_DIR}/assembly.fasta -o {DRAFT_DIR} -t {THREADS} -m {BASECALLER}",
+                                    shell=True, capture_output=True, text=True, check=True)
         outs.append(medaka_out)
 
         # MAP ILLUMINA READS ONTO MEDAKA CONSENSUS
-        os.mkdir(polish_dir)  # you have to create the output dir here
+        os.mkdir(POLISH_DIR)  # you have to create the output dir here
         # 1. INDEX ASSEMBLY
-        index_out = subprocess.run("bwa index %s/consensus.fasta" % draft_dir,
-                                   shell=True, capture_output=True, text=True)
+        index_out = subprocess.run(f"bwa index {DRAFT_DIR}/consensus.fasta",
+                                   shell=True, capture_output=True, text=True, check=True)
         outs.append(index_out)
         # 2. MAP READ SET 1
-        mem1_out = subprocess.run("bwa mem -t %i -a %s/consensus.fasta %s > %s/alignments_1.sam" %
-                                  (threads, draft_dir, short_reads_1, polish_dir),
-                                  shell=True, capture_output=True, text=True)
+        mem1_out = subprocess.run(f"bwa mem -t {THREADS} -a {DRAFT_DIR}/consensus.fasta {SHORT_READS_1} > {POLISH_DIR}/alignments_1.sam",
+                                  shell=True, capture_output=True, text=True, check=True)
         outs.append(mem1_out)
         # 3. MAP READ SET 2
-        mem2_out = subprocess.run("bwa mem -t %i -a %s/consensus.fasta %s > %s/alignments_2.sam" %
-                                  (threads, draft_dir, short_reads_2, polish_dir),
-                                  shell=True, capture_output=True, text=True)
+        mem2_out = subprocess.run(f"bwa mem -t {THREADS} -a {DRAFT_DIR}/consensus.fasta {SHORT_READS_2} > {POLISH_DIR}/alignments_2.sam",
+                                  shell=True, capture_output=True, text=True, check=True)
         outs.append(mem2_out)
 
         # POLYPOLISH
         # 1. INSERT FILTER
-        plp_insert_out = subprocess.run("polypolish_insert_filter.py --in1 %s/alignments_1.sam "
-                                        "--in2 %s/alignments_2.sam --out1 %s/filtered_1.sam --out2 %s/filtered_2.sam" %
-                                        (polish_dir, polish_dir, polish_dir, polish_dir),
-                                        shell=True, capture_output=True, text=True)
+        plp_insert_out = subprocess.run(f"polypolish_insert_filter.py --in1 {POLISH_DIR}/alignments_1.sam "
+                                        f"--in2 {POLISH_DIR}/alignments_2.sam --out1 {POLISH_DIR}/filtered_1.sam --out2 {POLISH_DIR}/filtered_2.sam",
+                                        shell=True, capture_output=True, text=True, check=True)
         outs.append(plp_insert_out)
         # 2. POLISH
-        plp_polish_out = subprocess.run("polypolish %s/consensus.fasta %s/filtered_1.sam %s/filtered_2.sam > "
-                                        "%s/assembly_flye_polished.fasta" %
-                                        (draft_dir, polish_dir, polish_dir, polish_dir),
-                                        shell=True, capture_output=True, text=True)
+        plp_polish_out = subprocess.run(f"polypolish {DRAFT_DIR}/consensus.fasta {POLISH_DIR}/filtered_1.sam {POLISH_DIR}/filtered_2.sam > "
+                                        f"{POLISH_DIR}/assembly_flye_polished.fasta",
+                                        shell=True, capture_output=True, text=True, check=True)
         outs.append(plp_polish_out)
 
         # REMOVE SAM FILES
         sam_files = ["alignments_1.sam", "alignments_2.sam", "filtered_1.sam", "filtered_2.sam"]
         for sam in sam_files:
-            os.remove(os.path.join(polish_dir, sam))
-        
+            os.remove(os.path.join(POLISH_DIR, sam))
         print("SAM files have been removed")
 
         # RENAME FLYE ASSEMBLY
-        os.rename("%s/assembly.fasta" % assembly_dir, "%s/assembly_flye_raw.fasta" % assembly_dir)
+        os.rename(f"{ASSEMBLY_DIR}/assembly.fasta", f"{ASSEMBLY_DIR}/assembly_flye_raw.fasta")
         # LINK ASSEMBLY TO RESULTS/ASSEMBLIES
         cwd = os.getcwd()
         # your polished assembly
-        source = os.path.join(cwd, "%s/assembly_flye_polished.fasta" % polish_dir)
+        source = os.path.join(cwd, f"{POLISH_DIR}/assembly_flye_polished.fasta")
         # same file as for unicycler
-        destination = os.path.join(cwd, "%s/assembly.fasta" % assembly_dir)
+        destination = os.path.join(cwd, f"{ASSEMBLY_DIR}/assembly.fasta")
         os.symlink(source, destination)
- 
+
     # PRINT STDOUT/STDERR TO LOG
     for out in outs:
-        # every out is subrocess.run output not string! 
+        # every out is subrocess.run output not string!
         print(out.stdout, out.stderr)
