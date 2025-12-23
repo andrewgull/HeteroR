fba75d8d774fcb32beca2cf0b67d8511e088b3a7
Andrei Guliaev <andrei.guliaev@imbim.uu.se>
2025-12-23 15:37:13 +0100

    chore: add utils.py

---
+++ workflow/scripts/utils.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from typing import Any
+
+
+def get_sample_path(wildcards: Any, data_frame: pd.DataFrame) -> str:
+    """
+    Get the path to the fastq file using its sample name.
+    arguments:
+        wildcards: wildcards object.
+        data_frame: A DataFrame object containing sample names and paths.
+    returns:
+        str: The path to the fastq file.
+    """
+    row = data_frame[data_frame["sample"] == wildcards.sample]
+    if not row.empty:
+        return row["path"].values[0]
+    else:
+        raise ValueError(f"Sample '{wildcards.sample}' not found in the provided DataFrame.")
+
+
+def tsv2dict(file_path: str) -> dict:
+    """
+    Read a TSV file and convert it to a dictionary.
+    arguments:
+        file_path: Path to the TSV file.
+    returns:
+        dict: A dictionary with parameters as keys and their values.
+    """
+    df = pd.read_csv(
+        file_path,
+        sep="\t",
+        dtype={"param": str, "value": str}
+    )
+    data_dict = df.set_index("param")["value"].to_dict()
+    return data_dict
+
+
+def final_files(workflow_type: str) -> Any:
+    """
+    Get the final files for a given workflow type.
+    arguments:
+        workflow_type: The type of workflow.
+    returns:
+        Any: The final files for the given workflow type.
+    """
+    if workflow_type == "assembly":
+        return expand(f"results/final/{{strain}}_{workflow_type}_all.done", strain=strains["strains"])
+    elif workflow_type == "annotation":
+        return expand(f"results/final/{{strain}}_{workflow_type}_all.done", strain=strains["strains"])
+    elif workflow_type == "mutants":
+        return expand(f"results/final/{{parent}}_{workflow_type}_all.done", parent=parents["parents"])
+    elif workflow_type == "phylogeny":
+        return expand(f"results/final/{{strain}}_{workflow_type}_all.done", strain=strains["strains"])
+    else:
+        raise ValueError(f"Unknown workflow type: {workflow_type}")
